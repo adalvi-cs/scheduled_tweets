c4093710b359e5e265f5c5f7495bfa30b1ef70d2
Amit Dalvi <adalvi@cubesmart.com>
2021-12-25 22:10:00 -0500

    part 6 changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # GET /about
-  get "about", to: "about#index"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # GET /about
+  get "about", to: "about#index"
+  get root to: "main#index"
+
 end
