5e119d1c9de011d15f34bcffdb37db9c7f89345a
Amit Dalvi <adalvi@cubesmart.com>
2022-01-19 14:09:23 -0500

    part 13 changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   # GET /about
   #get "about-us", to: "about#index", as: :about
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+    
+  post "sign_up", to: "registrations#create"
+
+
+
   root to: "main#index"
 
 end
