4dac5227b4bc3d0aceae84d9fd306e9de2bd574e
Amit Dalvi <adalvi@cubesmart.com>
2022-02-15 23:57:52 -0500

    parts 14-17 related changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
 class MainController < ApplicationController
   def index
-    #nothing
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Invalid email or password"
+    #hide flash messages keeping them only onetime for the one request
+    #flash.now[:notice] = "Logged in successfully"
+    #flash.now[:alert] = "Invalid email or password"
+    if session[:user_id]
+      # if user_id exists in session cookie, find user model
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,25 @@ class RegistrationsController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        #params
+        # #Parameters: {"authenticity_token"=>"[FILTERED]", "user"=>{"email"=>"yy@y.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+        #render plain: "Thanks!"
+        #render plain: params[:user]
+        @user = User.new(user_params)
+        if @user.save
+            #store user_id as a session cookie on server side
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end    
 end
